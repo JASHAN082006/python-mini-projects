7b9c7c54cc2883e7b936270b40c0901dfe3a8b75
JASHAN082006 <138104484+JASHAN082006@users.noreply.github.com>
2026-06-26 14:23:59 +0530

    Add files via upload
    A basic Python mini project that simulates an ATM system through the command line. The program allows users to log in using a PIN, check their account balance, deposit money, and withdraw funds. It is designed to demonstrate fundamental Python concepts such as variables, conditional statements, user input, and basic arithmetic operations.

---
+++ simple atm.py
@@ -0,0 +1,20 @@
+print("Simple ATM")
+balance = 5000
+pin = "1234"
+entered_pin = input("Enter your 4-digit PIN: ")
+if entered_pin == pin:
+    print("\n1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    choice = input("Choose an option (1, 2, or 3): ")
+    if choice == "1":
+        print(f"Balance: ₹{balance}")
+    elif choice == "2":
+        amount = int(input("Enter deposit amount: "))
+        balance = balance + amount
+        print(f"New Balance: ₹{balance}")
+    elif choice == "3":
+        amount = int(input("Enter withdrawal amount: "))
+        if amount <= balance:
+            balance = balance - amount
+            print(f"New Balance: ₹{balance}")
